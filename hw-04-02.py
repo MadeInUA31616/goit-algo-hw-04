7159db4eb732b2ac706a140eb70d31505d84032b
MadeInUA31616 <vladilechko31616@gmail.com>
2024-03-18 23:16:56 +0200

    commit2

--- hw-04-02.py
+++ hw-04-02.py
@@ -6,7 +6,7 @@ def get_cats_info(path):
                 cat_id, name, age = line.strip().split(',')
                 cats.append({'id': cat_id, 'name': name, 'age': int(age)})
     except FileNotFoundError:
-        print(f"File '{path}' not found.")
+        print(f"Файл '{path}' не знайдено.")
     return cats
 
 cats = get_cats_info("cats.txt")
