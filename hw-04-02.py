4b3c4339f060cc2094f2b375bd677b7699837ab3
MadeInUA31616 <vladilechko31616@gmail.com>
2024-03-18 23:10:12 +0200

    task 2

---
+++ hw-04-02.py
@@ -0,0 +1,13 @@
+def get_cats_info(path):
+    cats = []
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                cat_id, name, age = line.strip().split(',')
+                cats.append({'id': cat_id, 'name': name, 'age': int(age)})
+    except FileNotFoundError:
+        print(f"File '{path}' not found.")
+    return cats
+
+cats = get_cats_info("cats.txt")
+print(cats)
